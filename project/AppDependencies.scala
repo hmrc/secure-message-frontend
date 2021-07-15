7fed4df86a65a5015b01a52be51decee16adcbd0
masahmad <masahmad7@gmail.com>
2021-07-15 14:31:42 +0100

    add explicit import for charactercount component and updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -39,6 +39,7 @@ object AppDependencies {
     "com.typesafe.play"      %% "play-test"                % current          % Test,
     "uk.gov.hmrc"            %% "service-integration-test" % "0.13.0-play-27" % "test, it",
     "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0"          % "test, it",
+    "org.scalatestplus"      %% "mockito-3-4"              % "3.2.9.0"        % "test, it",
     "org.jsoup"              % "jsoup"                     % "1.14.1"         % "test, it",
     "org.mockito"            % "mockito-core"              % "3.11.2"         % "test, it",
     "com.vladsch.flexmark"   % "flexmark-all"              % "0.36.8"         % "test, it",
