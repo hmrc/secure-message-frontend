80ff8eebbf01dc5db60f66a150be957d239e154e
satyajosyula2020 <satya.josyula@digital.hmrc.gov.uk>
2023-06-22 20:22:01 +0100

    Update ConversationPartialISpec.scala

--- it/ConversationPartialISpec.scala
+++ it/ConversationPartialISpec.scala
@@ -111,11 +111,11 @@ class ConversationPartialISpec extends PlaySpec with ServiceSpec with MockitoSug
       replyPostResponse.status mustBe BAD_REQUEST
 
       val parsedContent = Jsoup.parse(replyPostResponse.body)
-      parsedContent.select("span#reply-form-error").text() mustBe "Error: The message must be 4,000 characters or fewer"
+      parsedContent.select("#reply-form-error").text() mustBe "Error: This field is required"
       val errorSummaryList =
         parsedContent.select("div.govuk-error-summary__body ul.govuk-error-summary__list")
       errorSummaryList.tagName("li").size() mustBe 1
-      errorSummaryList.select("li a").text() mustBe "The message must be 4,000 characters or fewer"
+      errorSummaryList.select("li a").text() mustBe "This field is required"
       errorSummaryList.select("li a").attr("href") mustBe "#reply-form"
     }
 
@@ -148,11 +148,11 @@ class ConversationPartialISpec extends PlaySpec with ServiceSpec with MockitoSug
       replyEmptyPostReponse.status mustBe BAD_REQUEST
 
       val parsedEmptyContent = Jsoup.parse(replyEmptyPostReponse.body)
-      parsedEmptyContent.select("span#reply-form-error").text() mustBe "Error: You must write a message to reply"
+      parsedEmptyContent.select("#reply-form-error").text() mustBe "Error: This field is required"
       val errorSummaryListEmptyContent =
         parsedEmptyContent.select("div.govuk-error-summary__body ul.govuk-error-summary__list")
       errorSummaryListEmptyContent.tagName("li").size() mustBe 1
-      errorSummaryListEmptyContent.select("li a").text() mustBe "You must write a message to reply"
+      errorSummaryListEmptyContent.select("li a").text() mustBe "This field is required"
       errorSummaryListEmptyContent.select("li a").attr("href") mustBe "#reply-form"
     }
   }
