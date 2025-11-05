c1cea18f2672f336de21699a069c401b72a07a16
tyagikumarvineet <22053543+tyagikumarvineet@users.noreply.github.com>
2025-11-05 16:49:03 +0000

    [DC-7967] Update the libraries' version to latest

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -14,15 +14,15 @@
  * limitations under the License.
  */
 
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.3.0"
+  private val bootstrapVersion = "10.4.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"    %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"    %% "play-frontend-hmrc-play-30" % "12.19.0",
+    "uk.gov.hmrc"    %% "play-frontend-hmrc-play-30" % "12.20.0",
     "uk.gov.hmrc"    %% "domain-play-30"             % "13.0.0",
     "org.typelevel"  %% "cats-core"                  % "2.13.0",
     "net.codingwell" %% "scala-guice"                % "6.0.0",
