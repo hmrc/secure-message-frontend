dd9c3452e4979ef8386443beee5f6f1d7516e4f8
tyagikumarvineet <22053543+tyagikumarvineet@users.noreply.github.com>
2025-12-12 15:28:36 +0000

    [DC-8229] Add test to assert Welsh contents

--- test/views/partials/MessageInboxSpec.scala
+++ test/views/partials/MessageInboxSpec.scala
@@ -20,7 +20,10 @@ import helpers.TestData.{ FIVE, TEST_CLIENT, TEST_HEADING, TEST_ID, TEST_NAME, T
 import models.{ Conversation, MessageHeader, MessageType }
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
-import play.api.i18n.Messages
+import play.api.Application
+import play.api.i18n.{ Lang, Messages, MessagesApi }
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.test.FakeRequest
 import play.api.test.Helpers.stubMessages
 import play.twirl.api.HtmlFormat
 import views.html.partials.messageInbox
@@ -74,6 +77,53 @@ class MessageInboxSpec extends PlaySpec with GuiceOneAppPerSuite {
             "</span>"
         )
       )
+    }
+
+    "display the correct contents in Welsh" in {
+
+      val msgHeader = MessageHeader(
+        messageType = MessageType.Conversation,
+        id = TEST_ID,
+        subject = TEST_SUBJECT,
+        issueDate = Instant.now(),
+        senderName = Some(TEST_NAME),
+        unreadMessages = true,
+        count = FIVE,
+        conversationId = Some(TEST_ID),
+        client = Some(TEST_CLIENT)
+      )
+
+      val msgInboxModel = MessageInbox(
+        clientService = TEST_SERVICE_NAME,
+        heading = TEST_HEADING,
+        total = FIVE,
+        unread = TWO,
+        conversationHeaders = List(msgHeader)
+      )
+
+      val application: Application = new GuiceApplicationBuilder()
+        .configure(
+          "microservice.metrics.enabled" -> false,
+          "metrics.enabled"              -> false,
+          "auditing.enabled"             -> false
+        )
+        .build()
+
+      implicit val lang: Lang = Lang.apply("cy")
+      implicit val messages: Messages =
+        application.injector.instanceOf[MessagesApi].preferred(FakeRequest().withTransientLang(lang))
+
+      val view: HtmlFormat.Appendable = application.injector.instanceOf[messageInbox].apply(msgInboxModel)
+
+      val viewBody: String = view.body
+
+      assert(viewBody.contains("<h1 class=\"govuk-heading-xl\">test_heading</h1>"))
+      messages("conversation.inbox.heading.message") must be("Neges")
+      messages("conversation.inbox.heading.date") must be("Dyddiad")
+      messages("conversation.inbox.heading.from") must be("oddi wrth")
+      messages("conversation.inbox.heading.subject") must be("Pwnc")
+      messages("conversation.inbox.subject.count") must be("o negeseuon yn y sgwrs hon")
+      messages("conversation.inbox.heading.status") must be("Statws")
     }
   }
 
